022de4e34f447a1ebf7dd4bb7010fa721c451862
tomi-fic <59510664+tomi-fic@users.noreply.github.com>
2021-02-13 19:35:40 +0100

    FE - Product page view

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,28 +1,24 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <LinkContainer to='/'>
-            <Navbar.Brand href='/'>eShop</Navbar.Brand>
-          </LinkContainer>
+          <NavLink to='/' className='nav-link'>
+            <Navbar.Brand>eShop</Navbar.Brand>
+          </NavLink>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ml-auto'>
-              <LinkContainer to='/cart'>
-                <Nav.Link>
-                  <i className='fas fa-cart-plus'></i> Cart
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to='/login'>
-                <Nav.Link>
-                  <i className='fas fa-user'></i> Sign In
-                </Nav.Link>
-              </LinkContainer>
+            <Nav className='ml-auto' defaultActiveKey='/'>
+              <NavLink to='/cart' className='nav-link'>
+                <i className='fas fa-cart-plus'></i> Cart
+              </NavLink>
+              <NavLink to='/login' className='nav-link'>
+                <i className='fas fa-user'></i> Sign In
+              </NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/views/ProductView.jsx
+++ frontend/src/views/ProductView.jsx
@@ -1,7 +1,72 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import RatingStars from '../components/RatingStars'
+import products from '../products'
 
-const ProductView = () => {
-  return <div>product info</div>
+const ProductView = (props) => {
+  // will be replaced by BE logic
+  const prod = products.find((p) => p._id === props.match.params.id)
+
+  return (
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={prod.image} alt={prod.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h4>{prod.name}</h4>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <RatingStars rating={prod.rating} numReviews={prod.numReviews} />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: {prod.price}€</ListGroup.Item>
+            <ListGroup.Item>
+              <div style={{ textAlign: 'center' }}>{prod.description}</div>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>{prod.price}€</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Stock:</Col>
+                  <Col>
+                    {prod.countInStock > 0
+                      ? `${prod.countInStock} pcs`
+                      : 'Out of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className='btn-block'
+                  type='button'
+                  disabled={prod.countInStock === 0}
+                >
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
 }
 
 export default ProductView
